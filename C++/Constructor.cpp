2c2217ba205df5d9fd2521e9175e0c3188866004
RizviBR0 <rizvibd1100@gmail.com>
2024-03-03 20:58:10 +0600

    modified:   C++/Constructor.cpp

--- C++/Constructor.cpp
+++ C++/Constructor.cpp
@@ -2,26 +2,36 @@
 #include <stdlib.h>
 using namespace std;
 
+// Class definition
 class student
 {
     public:
     int id;
     double gpa;
 
+    // Member function
     void display()
     {
         cout << id << " " << gpa << endl;
     }
 
+    // Parameterized constructor
     student(int x, double y)
     {
         id = x;
         gpa = y;
     }
+
+    // Default constructor
+    student()
+    {
+        cout << "Default constructor" << endl;
+    }
 };
 
 int main()
 {
+    student dcon;
     student alim(101, 3.45);
     student jalim(101, 3.45);
     alim.display();
