93c3240c78e5e0f49c14ee2f5f3e42e4e4b20936
RizviBR0 <rizvibd1100@gmail.com>
2024-05-28 18:32:38 +0600

    new file:   C/Element-insertion.c

--- C++/code.cpp
+++ C++/code.cpp
@@ -1,34 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void Line()              //Function[1]
+template <class myTemplate1, class myTemplate2>
+
+myTemplate1 add(myTemplate1 a, myTemplate2 b)
 {
-    for(int L=1; L<=80; L++) cout<<"-";
-    cout<<endl;
+    return a+b;
 }
 
-void Line(int N)         //Function[2]
-{
-    for(int L=1; L<=N; L++) cout<<"*";
-    cout<<endl;
-}
-                                  
-void Line(char C, int N)         //Function[3]
-{
-    for(int L=1; L<=N; L++) cout<<C;
-    cout<<endl;
-}
-
-void Line(int M, int N)         //Function[4]
-{
-    for(int L=1; L<=N; L++) cout<<M*L;
-    cout<<endl;
-}
-
-int main()
-{
-    int A=9, B=4, C=3;
-    char K='#';
-    Line(K, B);
-    Line(A, C);
+int main() {
+    cout << add(1, 2) << endl;
 }
