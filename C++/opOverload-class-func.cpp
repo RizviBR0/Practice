f235e337bc1f495b16b69df7f5366b9a9bfd6d58
RizviBR0 <rizvibd1100@gmail.com>
2024-05-30 13:03:50 +0600

    new file:   C++/opOverload-class-func.cpp 	new file:   C++/opOverload-friend-func.cpp

---
+++ C++/opOverload-class-func.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class demo
+{
+    int a, b;
+
+public:
+    demo(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    void show()
+    {
+        cout << "a=" << a << " b=" << b << endl;
+    }
+
+    void operator-()
+    {
+        a = -a;
+        b= -b;
+    }
+};
+
+int
+main()
+{
+    demo d(-10, 20);
+    d.show();
+    -d;
+    d.show();
+}
---
+++ C++/opOverload-friend-func.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class demo
+{
+    int a, b;
+
+public:
+    demo(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    void show()
+    {
+        cout << a << " " << b << endl;
+    }
+
+    friend void operator-(demo &d);
+};
+
+void operator-(demo &d)
+{
+    d.a = -d.a;
+    d.b = -d.b;
+}
+
+int main()
+{
+    demo d(10, -20);
+    d.show();
+    -d;
+    d.show();
+}
