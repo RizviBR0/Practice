89c4b249b0a478e1fb0979f686964d31bc6f40fe
RizviBR0 <rizvibd1100@gmail.com>
2025-02-15 23:14:15 +0600

    modified

--- Python/code.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-uppercase_letter = list(string.ascii_uppercase)
-lowercase_letter = list(string.ascii_lowercase)
-
-letters = uppercase_letter + lowercase_letter
-
-print(f"Your password is : {letters}")
